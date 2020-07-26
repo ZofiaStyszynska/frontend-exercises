48982d45ccfdd4732ab6301d5cb578281476ecd0
Piotr Jablonski <jabl.piotrek@gmail.com>
2020-07-26 12:18:39 +0200

    3-g

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-function showAlert(text) {
-    window.alert(text || 'Default text');
+function showAlert(text1, text2) {
+
+    window.alert(text1 + ' , ' + text2);
 }
 
 // ######################
 
 window.addEventListener('load', () => {
     const button = document.getElementById('info-button');
-    const input = document.getElementById('info-input');
-    
+    const input1 = document.getElementById('info-input-1');
+    const input2 = document.getElementById('info-input-2');
+
     button.addEventListener('click', () => {
-        const text = input.value
-        if (text) {
-            showAlert(text)
-            input.value = '';
+        const text1 = input1.value
+        const text2 = input2.value
+
+        if (text1 || text2) {
+            showAlert(text1, text2)
+            input1.value = '';
+            input2.value = '';
         }
     })
 
-    input.addEventListener('keyup', (e) => {
-        button.disabled = e.target.value.length >= 3 ? false : true;
-    })
+    // input1.addEventListener('keyup', (e) => {
+    //     // button.disabled = e.target.value.length >= 3 ? false : true;
+    // })
 });
