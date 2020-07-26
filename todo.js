ee27d30674ab009bbb4446065b3adfdbdaf98a6e
Piotr Jablonski <jabl.piotrek@gmail.com>
2020-07-26 14:39:32 +0200

    4-e

--- todo.js
+++ todo.js
@@ -1,5 +1,6 @@
-function addNote(text, container) {
+function addNote(text, container, index) {
     const note = document.createElement('div');
+    note.id = `note-${index}`
     note.innerHTML = `<p>${text}</p> <button type="button">Delete</button>`;
     container.appendChild(note);
 }
@@ -12,18 +13,25 @@ function clearNotes(container) {
     container.innerHTML = '';
 }
 
+function deleteNote(noteId) {
+    const noteToDelete = document.getElementById(`note-${noteId}`);
+    noteToDelete.remove();
+}
+
 window.addEventListener('load', () => {
     const addNoteButton = document.getElementById('add-note');
     const clearNotesButton = document.getElementById('clear-notes');
     const noteInput = document.getElementById('note-input');
     const notesContainer = document.getElementById('notes-container');
+    let index = 0;
 
     addNoteButton.addEventListener('click', () => {
         const text = noteInput.value;
 
         if (text) {
-            addNote(text, notesContainer);
+            addNote(text, notesContainer, index);
             clearInput(noteInput);
+            index++;
         }
     });
 
