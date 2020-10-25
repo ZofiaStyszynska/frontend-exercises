ad6e58ac80b5de15b2ed457a440c5a82521f709a
Piotr Jablonski <jabl.piotrek@gmail.com>
2020-10-25 12:31:40 +0100

    add hide button

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 window.addEventListener('load', () => {
-    showMainSection();
+  const showButton = document.querySelector('#show-article');
+  const hideButton = document.querySelector('#hide-article');
+  
+  showButton.addEventListener('click', () => {
+    toggleMainSection(true);
+  });
+
+  hideButton.addEventListener('click', () => {
+    toggleMainSection(false);
+  });
 });
 
-function showMainSection() {
-    const mainSection = document.querySelector('.article__main');
-    mainSection.style.display = 'block';
+function toggleMainSection(show) {
+  const mainSection = document.querySelector('.article__main');
+
+  mainSection.style.display = show ? 'block' : 'none'
 }
